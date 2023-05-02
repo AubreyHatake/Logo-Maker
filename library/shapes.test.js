2b8a0f773248007119b1b3befe3df8fbbf8deaa2
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-05-01 18:55:30 -0600

    adding to my shapes test

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -1,1 +1,10 @@
-
+// using this code to test a circle 
+describe("Circle test", () => {
+    test("test for a circle with a #ca00ca background", () => {
+      const shape = new Circle();
+      shape.setColor("#ca00ca");
+      expect(shape.render()).toEqual(
+        '<circle cx="150" cy="115" r="80" fill="#ca00ca" />'
+      );
+    });
+  });
